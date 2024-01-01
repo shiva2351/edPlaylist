15ef85421ced301f6ce19f0c8b97891c87056c52
shiva <smpgr2351@gmail.com>
2024-01-01 03:51:19 +0000

    musicplayList

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+import TracksList from './components/TracksList'
+
+// Replace your code here
+
+const App = () => <TracksList />
+
+export default App
---
+++ src/components/TracksList/StyledComponents.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+export const ImageTrack = styled.img`
+  width: 100%;
+`
+export const UnOrderList = styled.ul`
+  padding-left: 0;
+`
+export const ListItem = styled.li`
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+  border: 2px solid red;
+  justify-content: space-between;
+  align-items: center;
+`
+export const Header = styled.div`
+  width: 100%;
+  background-image: url('https://assets.ccbp.in/frontend/react-js/music-playlist/music-playlist-Edsheeran-bg.png');
+  background-size: cover;
+`
+export const ListFirst = styled.div`
+  border: 2px solid green;
+  display: flex;
+  flex-basis: 120px;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const Listsec = styled.div`
+  border: 2px solid yellow;
+  display: flex;
+  flex-basis: 120px;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const ImageList = styled.img`
+  width: 100px;
+`
